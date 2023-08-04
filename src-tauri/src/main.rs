f9cb59d7af879a061f0ef3214a42f297af0ad83e
zhouyuan63 <zhouyuan11@jd.com>
2023-08-04 14:59:52 +0800

    add: 增加退出时关闭服务

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,24 +4,19 @@
 use std::env;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
-use tauri::{SystemTray, WindowEvent};
+use tauri::{SystemTray};
 use std::process::{Command, Child};
 use tauri::Manager;
 use tauri::{CustomMenuItem,SystemTrayMenu, SystemTrayMenuItem,SystemTrayEvent};
+use lazy_static::lazy_static;
 
-
-#[tauri::command]
-async fn greet(_app_handle: tauri::AppHandle) -> String{
-    return "hello".to_string();
+lazy_static! {
+  static ref CHILD_ID: Arc<Mutex<Option<Child>>> = Arc::new(Mutex::new(None));
 }
 
 fn main() {
     let app = tauri::Builder::default();
 
-    // 注册url协议
-    // if let Err(ref e) = install_schema() {
-    //     println!("error: {}", e);
-    // }
     tauri_plugin_deep_link::prepare("com.yintao.jd");
 
     // 添加托盘
@@ -33,57 +28,56 @@ fn main() {
       .add_item(hide);
   
     let system_tray = SystemTray::new().with_menu(tray_menu);
-  
+
     app
       .system_tray(system_tray)
-      .on_system_tray_event(|app, event| menu_handle(app, event))
-      .setup(|_app: &mut tauri::App| {
+      .setup(move |app| {
+        let app_handle = app.app_handle();
 
-        let resource_path = _app.path_resolver()
-        .resolve_resource("binaries/cherry-server-macos-aarch64-apple-darwin")
-        .expect("failed to resolve resource");
+        let child_process = start_yintao_server(app_handle);
+        *CHILD_ID.lock().unwrap() = Some(child_process);
 
-        let child_process = start_child_process(resource_path);
-        let child_process_clone = child_process.clone();
-   
-        _app.manage(move |window: &mut tauri::Window| {
-            let child_handle = child_process_clone.clone();
-            window.on_window_event(move |event| {
-                match event {
-                    WindowEvent::CloseRequested { .. } => {
-                        println!("Window closed!");
-                        // 在主进程退出时结束子进程
-                        if let Some(mut child) = child_handle.lock().expect("Failed to lock child process").take() {
-                            child.kill().expect("Failed to kill child process");
-                            child.wait().expect("Failed to wait for child process");
-                        }
-                    }
-                    _ => {}  // 其他事件
-                }
-            });
-        });
-
-        println!("File exists!");
-
-        // let handle: tauri::AppHandle = app.handle();
         tauri_plugin_deep_link::register(
-            "yintao",
-            move |request| {
-                dbg!(&request);
-                println!("收到协议调用");
-              },
-          )
-          .unwrap(/* If listening to the scheme is optional for your app, you don't want to unwrap here. */);
-        Ok(())
+          "yintao",
+          move |request| {
+              dbg!(&request);
+              println!("收到协议调用");
+            },
+        )
+        .unwrap(/* If listening to the scheme is optional for your app, you don't want to unwrap here. */);
 
-    }).run(tauri::generate_context!())
-      .expect("failed to run app");
+        Ok(())
+    })
+      .on_system_tray_event(|app, event| menu_handle(app, event))
+      .build(tauri::generate_context!())
+      .expect("error while running tauri application")
+      .run(|_app_handle, event| match event {
+        tauri::RunEvent::ExitRequested { api, .. } => {
+          println!("kill yintao server!");
+          if let Some(mut child_id) = CHILD_ID.lock().unwrap().take() {
+            let _ = child_id.kill();
+          }
+        api.prevent_exit();
+        },
+        tauri::RunEvent::Exit => {
+          _app_handle.exit(0);
+      },
+        _ => {}
+      });
+
 }
 
-fn start_child_process(resource_path:PathBuf) -> Arc<Mutex<Option<Child>>> {
-    let child = Command::new(resource_path).spawn().expect("Failed to start child process");
-    // 使用 Arc<Mutex<Option<_>>> 确保子进程可以在线程间共享和安全地关闭
-    Arc::new(Mutex::new(Some(child)))
+
+fn start_yintao_server(app: tauri::AppHandle) -> Child {
+    let resource_path: PathBuf = app.path_resolver()
+    .resolve_resource("binaries/cherry-server-macos-aarch64-apple-darwin")
+    .expect("failed to resolve resource");
+
+    let child = Command::new(resource_path)
+    .spawn()
+    .expect("Failed to start long running service");
+
+    child
 }
 
 fn menu_handle(_app_handle: &tauri::AppHandle, event: SystemTrayEvent) {
